202c18f78adb4d83fd1ce29b82642bfa26175098
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-11 16:54:25 -0400

    delete user story 2 from spec/features/authors/index_spec.rb since it's a different feature from the index

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -14,19 +14,4 @@ RSpec.describe "/authors (Author Index Page)", type: :feature do
       expect(page).to have_content(author_2.name)
     end
   end
-
-  # #user story 2
-  # describe "as a visitor, when I visit author/:id page" do
-  #   let!(:author_1) { Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )}
-  #   let!(:author_2) { Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )}
-    
-  #   it "the Author of that id and all its attributes" do
-  #     visit "/authors/#{author_1.id}"
-  #     # save_and_open_page
-  #     expect(page).to have_content("#{author_1.name}")
-  #     expect(page).to have_content("Currently Alive: #{author_1.currently_alive}")
-  #     expect(page).to have_content("Age: #{author_1.age}")
-  #     expect(page).to have_content("ID: #{author_1.id}")
-  #   end
-  # end
 end
