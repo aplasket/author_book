af6094d66beead56b3558539c071c52cba46e39b
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-10 20:04:02 -0400

    add route to authors, add author controller, add authors index view, add tests to index_spec in features

---
+++ spec/features/authors/index_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "/authors (Author Index Page)", type: :feature do
+  #user story 1
+  describe "as a visitor, when I visit author index page" do
+    let!(:author_1) { Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )}
+    let!(:author_2) { Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )}
+
+    it "I see the name of each author" do
+      visit "/authors"
+      
+      expect(page).to have_content(author_1.name)
+      expect(page).to have_content(author_2.name)
+    end
+  end
+end
