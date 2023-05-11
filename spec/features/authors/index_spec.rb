b25d97c3620eea8bf837fdb071af729c44f3c8d7
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-11 14:20:10 -0400

    add route, view and tests for author # show page

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe "/authors (Author Index Page)", type: :feature do
       expect(page).to have_content(author_2.name)
     end
   end
+
+  describe "as a visitor, when I visit author/:id page" do
+    let!(:author_1) { Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )}
+    let!(:author_2) { Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )}
+    
+    it "the Author of that id and all its attributes" do
+      visit "/authors/#{author_1.id}"
+      # save_and_open_page
+      expect(page).to have_content("#{author_1.name}")
+      expect(page).to have_content("Currently Alive: #{author_1.currently_alive}")
+      expect(page).to have_content("Age: #{author_1.age}")
+    end
+  end
 end
