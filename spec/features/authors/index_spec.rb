878533f9bb727523cbfd4b7b8317269396553932
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-11 18:55:21 -0400

    add order function to author index page and add created at timestamp visible

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -13,5 +13,28 @@ RSpec.describe "/authors (Author Index Page)", type: :feature do
       expect(page).to have_content(author_1.name)
       expect(page).to have_content(author_2.name)
     end
+
+  end
+
+  #user story 6 add on
+  describe "as a visitor, I see authors organized by creation date" do
+    before(:each) do
+      @author_1 = Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )
+      @author_2 = Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )
+      @author_3 = Author.create!(name: "Brene Brown", currently_alive: true, age: 57 )
+    end
+
+    # let!(:author_1) { Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )}
+    # let!(:author_2) { Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )}
+    # let!(:author_3) { Author.create!(name: "Brene Brown", currently_alive: true, age: 57 )}
+
+    it "shows when each author was created, sorted by most recent creation date" do
+      visit "/authors"
+
+      save_and_open_page
+
+      expect(@author_3.name).to appear_before(@author_2.name)
+      
+    end
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Author, type: :model do
   describe "relationships" do
     it { should have_many :books }
   end
+
+  # As a visitor
+  # When I visit the parent index,
+  # I see that records are ordered by most recently created first
+  # And next to each of the records I see when it was created
+  
+  describe "#sort_by_creation" do
+    let!(:author_1) { Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )}
+    let!(:author_2) { Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )}
+    let!(:author_3) { Author.create!(name: "Brene Brown", currently_alive: true, age: 57 )}
+
+    it "sorts parent index by creation date and lists when it was created" do
+      expected = [author_3, author_2, author_1]
+      expect(Author.sort_by_creation).to eq(expected)
+    end
+  end
 end
