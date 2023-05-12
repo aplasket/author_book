c05e1948ea7baf8c085651db93b7e7c1ee008d61
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-11 20:36:28 -0400

    update readme, fix controller so view is in order for author index

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -18,22 +18,16 @@ RSpec.describe "/authors (Author Index Page)", type: :feature do
 
   #user story 6 add on
   describe "as a visitor, I see authors organized by creation date" do
-    before(:each) do
-      @author_1 = Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )
-      @author_2 = Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )
-      @author_3 = Author.create!(name: "Brene Brown", currently_alive: true, age: 57 )
-    end
-
-    # let!(:author_1) { Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )}
-    # let!(:author_2) { Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )}
-    # let!(:author_3) { Author.create!(name: "Brene Brown", currently_alive: true, age: 57 )}
+    let!(:author_1) { Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )}
+    let!(:author_2) { Author.create!(name: "Maya Angelou", currently_alive: false, age: 86 )}
+    let!(:author_3) { Author.create!(name: "Brene Brown", currently_alive: true, age: 57 )}
 
     it "shows when each author was created, sorted by most recent creation date" do
       visit "/authors"
 
-      save_and_open_page
+      # save_and_open_page
 
-      expect(@author_3.name).to appear_before(@author_2.name)
+      expect(author_2.name).to appear_before(author_1.name)
       
     end
   end
