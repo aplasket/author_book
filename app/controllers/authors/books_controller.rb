97e2a9984411a2e1aa2311049e53f94206d65e9c
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-16 16:29:12 -0400

    add method to author/books controller to filter by price

--- app/controllers/authors/books_controller.rb
+++ app/controllers/authors/books_controller.rb
@@ -1,27 +1,33 @@
 class Authors::BooksController < ApplicationController
   def index
     @author = Author.find(params[:author_id])
-
-    if params[:sort] == "AZ"
-      @books = @author.sort_books
-    else
-      @books = @author.books
-    end
+    @books = get_books
   end
-
+  
+  
   def new
     @author = Author.find(params[:author_id])
     @book = Book.new
   end
-
+  
   def create
     author = Author.find(params[:author_id])
     author.books.create!(book_params)
     redirect_to "/authors/#{author.id}/books"
   end
-
+  
   private
-    def book_params
-      params.permit(:title, :price, :rating, :purchasable_online)
+  def book_params
+    params.permit(:title, :price, :rating, :purchasable_online)
+  end
+  
+  def get_books
+    if !params[:price].nil?
+      @author.books_over_price(params[:price])
+    elsif params[:sort] == "AZ"
+      @author.sort_books
+    else
+      @author.books
     end
+  end
 end
