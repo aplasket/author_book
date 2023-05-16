fe0da5e8c6441c5397a92f2a4673aab82519f9bf
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-15 20:44:32 -0400

    add feature to update book details

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,19 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to "/books/#{params[:id]}"
+  end
+
+  private
+    def book_params
+      params.permit(:title, :price, :rating, :purchasable_online)
+    end
 end
