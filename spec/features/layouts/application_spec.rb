fdc76a6f5d74478580ba76a9ed58b0b293dd83ff
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-16 17:02:09 -0400

    delete out user story comments

--- spec/features/layouts/application_spec.rb
+++ spec/features/layouts/application_spec.rb
@@ -1,11 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "application layout view", type: :feature do
-    #user story 8
-  #As a visitor
-  #When I visit any page on the site
-  #Then I see a link at the top of the page that takes me to the Child Index
-
+  #user story 8
   describe "link to child index" do
     let!(:author_1) { Author.create!(name: "J.K. Rowling", currently_alive: true, age: 57 )}
 
@@ -26,10 +22,8 @@ RSpec.describe "application layout view", type: :feature do
       click_link("See All Books")
       expect(current_path).to eq("/books")
     end
+
     #user story 9
-    #As a visitor
-    #When I visit any page on the site
-    #Then I see a link at the top of the page that takes me to the Parent Index
     it "can visit any page on the site and see the author index" do
       visit "/authors/#{author_1.id}/books"
       expect(page).to have_link("See All Authors")
